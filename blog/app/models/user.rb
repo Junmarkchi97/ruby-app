a63e2146565c0aa6c839cc3d2773b7e0cfd84eaa
Junmark Chi <chijunmark97@gmail.com>
2022-08-15 07:01:02 +0800

    Update files

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true, length: { minimum: 3 }
-  validates :age, presence: true, 
+  validates :age, presence: true,
 end
